8f3b07884afabacfcae0dc149a306ae43f9e9765
Stef Spears <spears.sj87@gmail.com>
2019-07-09 04:03:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,9 @@
 first_number = 35
 second_number = 2 
 
-def calculator(first_number, second_number)
+
   sum = first_number+second_number
   difference = first_number-second_number
   product = first_number*second_number
   quotient = first_number/second_number
-  puts "Sum: #{sum}; Difference: #{difference}; Product: #{product}; Quotient: #{quotient}"
-end
+ 
